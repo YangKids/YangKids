1b36a2f197264ce98c84b311c9d96ea371cde6c7
Jiwon <kimjw3928@gmail.com>
2023-05-05 22:09:00 +0900

    [#4]변수몀 변경(id->articleId), resultMap 삭제

--- src/main/java/com/yangkids/model/service/ArticleService.java
+++ src/main/java/com/yangkids/model/service/ArticleService.java
@@ -7,17 +7,15 @@ import com.yangkids.model.dto.SearchCondition;
 
 //사용자 친화적으로
 public interface ArticleService {
-	//게시글 전체 조회
-	public List<Article> getArticleList();
 	
 	//게시글 상세조회 (클릭시 읽는거)
-	public Article readArticle(int id); 
+	public Article readArticle(int articleId); 
 	
 	//게시글 작성
 	public void writeArticle(Article article);
 	
 	//게시글 삭제 
-	public void removeArticle(int id);
+	public void removeArticle(int articleid);
 	
 	//게시글 수정
 	public void modifyArticle(Article article);
